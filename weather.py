32b2b18ca8dae377197970a6f5753b6241f55379
Kurush Dubash <dubash.kurush@gmail.com>
2014-08-12 00:37:07 -0700

    added zip code to city lookup, minor updates to wind speed

--- weather.py
+++ weather.py
@@ -2,8 +2,7 @@ import requests
 
 wind_directions = {'S': 'South', 'N':'North', 'W': 'West', 'E': 'East', 'SW' : 'South West', 'NW': 'North West', 'NE': 'North East', 'SE': 'South East',}
 
-def get_weather_json():
-
+def get_zip_code():
 	print('Please enter your zip code:')
 	zip_code = input()
 
@@ -11,26 +10,39 @@ def get_weather_json():
 		print('Invalid Zip Code')
 		print('Please enter your zip code:')
 		zip_code = input()
+	return zip_code
 
-	url = 'http://www.myweather2.com/developer/forecast.ashx?uac=xuv-N8e7ge&output=json&query=' + zip_code + '&temp_unit=f&ws_unit=mph'
-
+def get_weather_json(zip):
+	url = 'http://www.myweather2.com/developer/forecast.ashx?uac=xuv-N8e7ge&output=json&query=' + zip + '&temp_unit=f&ws_unit=mph'
 	weather_data = requests.get(url)
 	weather_json = weather_data.json()
 	return weather_json
 
+def get_location_info(zip):
+	url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + str(zip) + '&sensor=true'
+	location_data = requests.get(url)
+	location_json = location_data.json()
+	return location_json['results'][0]['formatted_address']
+
 def get_temperature(json_data):
 	temp = json_data['weather']['curren_weather'][0]['temp']
-	return str(temp) + ' degrees fahrenheit / ' + str(int((int(temp) - 32) / 1.8)) + ' degrees celcius'
+	return str(temp) + ' degrees fahrenheit / ' + str(int((int(temp) - 32) / 1.8)) + ' degrees celsius'
 
 def get_wind_data(json_data):
 	wind_speed = json_data['weather']['curren_weather'][0]['wind']
 	return wind_speed
 
-data = get_weather_json() 
+zip_code_input = get_zip_code()
+data = get_weather_json(zip_code_input) 
+
 print()
+#Get Zip Code City, State, and Country info
+print(get_location_info(zip_code_input))
+#Get Temperature 
 print('Temperature:' , get_temperature(data))
-wind_data = get_wind_data(data) #Collect all wind data
-if wind_data[0]['dir'] != "Not Available": #If Weather is avalible, show it
+#Collect all wind data
+wind_data = get_wind_data(data) 
+if wind_data[0]['dir'] != "Not Available" and wind_data[0]['dir'] in wind_directions: #If Weather is avalible, show it
 	wind_dire = get_wind_data(data)[0]['dir']
 	print('Wind Speed:', get_wind_data(data)[0]['speed'] + 'MPH' , wind_directions[wind_dire])
 print(data['weather']['curren_weather'][0]['weather_text']) #Weather Description
